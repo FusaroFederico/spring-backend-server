63b0ec1dd77870698624f85358c873472c84ec3f
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-12-10 10:44:14 +0100

    Initial commit

---
+++ src/main/java/com/server/backend/spring/SpringBackendServerApplication.java
@@ -0,0 +1,13 @@
+package com.server.backend.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendServerApplication.class, args);
+	}
+
+}
